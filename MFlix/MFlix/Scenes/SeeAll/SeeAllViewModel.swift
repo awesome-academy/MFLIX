28595d9132a0c5cf177adabeb05387fce84b8573
anhnv-0990 <ngo.viet.anh@sun-asterisk.com>
2020-05-13 09:04:13 +0700

    Create UI for SeeAll Screen

---
+++ MFlix/MFlix/Scenes/SeeAll/SeeAllViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  SeeAllViewModel.swift
+//  MFlix
+//
+//  Created by Viet Anh on 5/11/20.
+//  Copyright © 2020 VietAnh. All rights reserved.
+//
+
+struct SeeAllViewModel {
+    let navigator: SeeAllNavigatorType
+    let useCase: SeeAllUseCaseType
+    let type: CategoryType
+}
+
+extension SeeAllViewModel: ViewModelType {
+    struct Input {
+        let loadTrigger: Driver<Void>
+    }
+    
+    struct Output {
+        let title: Driver<String>
+    }
+    
+    func transform(_ input: Input) -> Output {
+        
+        let title = input.loadTrigger
+            .map{ _ in self.type.title }
+            .asDriver()
+        
+        return Output(
+            title: title
+        )
+    }
+}
