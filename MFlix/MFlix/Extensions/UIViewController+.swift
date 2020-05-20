41a1cf980f5a2870e77f7729a5f79e53c1d13a26
anhnv-0990 <ngo.viet.anh@sun-asterisk.com>
2020-05-20 14:51:27 +0700

    Favorite Screen

--- MFlix/MFlix/Extensions/UIViewController+.swift
+++ MFlix/MFlix/Extensions/UIViewController+.swift
@@ -17,4 +17,30 @@ extension UIViewController {
         ac.addAction(okAction)
         present(ac, animated: true, completion: nil)
     }
+    
+    func showAlertView(title: String?,
+                       message: String?,
+                       style: UIAlertController.Style,
+                       actions: [(String, UIAlertAction.Style)]) -> Observable<Int> {
+        return Observable.create { observer in
+            let alertController = UIAlertController(title: title,
+                                                    message: message,
+                                                    preferredStyle: style)
+            
+            actions.enumerated().forEach { index, action in
+                let action = UIAlertAction(title: action.0,
+                                           style: action.1) { _ in
+                                            observer.onNext(index)
+                                            observer.onCompleted()
+                }
+                alertController.addAction(action)
+            }
+            self.present(alertController,
+                         animated: true,
+                         completion: nil)
+            return Disposables.create {
+                alertController.dismiss(animated: true, completion: nil)
+            }
+        }
+    }
 }
